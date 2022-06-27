20495a01160c1c8e17542d1815abd1daff065d16
jerry199507 <108249680+jerry199507@users.noreply.github.com>
2022-06-27 11:48:53 +0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -11,14 +11,3 @@ MONGO_CONN_STR = 'xxxxx'
 VIDEO_PATH = '/data/app/videos'
 EXCEL_PATH = '../../excel'
 
-# 日志配置
-file_name = os.path.basename(sys.argv[0]).replace('.py', '')
-logging.basicConfig(level=logging.ERROR,
-                    filename=f'../../log/{file_name}.log',
-                    filemode='a',
-                    format=
-                    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-                    )
-console = logging.StreamHandler()
-console.setLevel(logging.ERROR)
-logging.getLogger('').addHandler(console)
